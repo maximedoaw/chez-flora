bd639ed9a0f2f8555fc9b3ed1606970f88679855
maximedoaw <maximedoaw204@gmail.com>
2025-03-21 18:22:01 +0100

    sa devrait etre bon j'espere

--- app/blog/[slug]/page.tsx
+++ app/blog/[slug]/page.tsx
@@ -1,5 +1,5 @@
 import BlogPost from "@/components/blog/BlogPost"
 
-export default async function BlogPostPage({ params }: { params: { slug: string } }) {
+export default async function BlogPostPage({ params }: { params: { slug: any } }) {
   return <BlogPost slug={params.slug} />
 }
